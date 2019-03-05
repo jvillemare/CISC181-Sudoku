c7d79dd8cc633aed9913672870fd916a8006df45
James Villemarette <jvill@udel.edu>
2019-03-05 17:51:30 -0500

    adding master lab1 code from professor

--- src/test/java/pkgHelper/LatinSquareTest.java
+++ src/test/java/pkgHelper/LatinSquareTest.java
@@ -9,165 +9,116 @@ import org.junit.Test;
 public class LatinSquareTest {
 
 	@Test
-	public void test() {
-		//fail("Not yet implemented");
-	}
-	
-	@Test
-	public void makeLatinSquareTest1() {
-		LatinSquare ls = new LatinSquare();
-		
-		assertEquals(true, ls instanceof Object);
-	}
-	
-	@Test
-	public void containsZeroTest1() {
-		
-		int[][] data1 = {{1, 2}, {3, 4}};
-		int[][] data2 = {{5, 6}, {0, 8}};
-		
-		LatinSquare noZeros  = new LatinSquare(data1);
-		LatinSquare hasZeros = new LatinSquare(data2);
-		
-		assertEquals(false, noZeros.ContainsZero());
-		assertEquals(true, hasZeros.ContainsZero());
-		
-		
-	}
-	
-	@Test
-	public void doesElementExistTest1() {
-		
-		int[] arr1 = {0, 1, 2, 3, 4, 5};
-		int iValue = 2;
-		
-		assertEquals(true, LatinSquare.doesElementExist(arr1, iValue));
-		assertEquals(false, LatinSquare.doesElementExist(arr1, 6));
-		
-	}
-	
-	@Test
-	public void getColumnTest1() {
-		
-		int[][] latinSquare1 = {{1, 2},{3, 4}};
-		int[] col1 = {1, 3};
-		
-		LatinSquare c = new LatinSquare(latinSquare1);
-		
-		assertEquals(true, Arrays.equals(col1, c.getColumn(0)));
-		
-	}
-	
-	@Test
-	public void getLatinSquareTest1() {
-		// TODO: Yisi will make this test.
-		
-		int[][] LS1 = {{1, 2},{3, 4}};
-		LatinSquare data3 = new LatinSquare(LS1);
-		assertEquals(LS1, data3.getLatinSquare());
-		
-	}
-	
-	@Test
-	public void getRowTest1() {
-		// TODO: Yifan will make this test.
-		
-		int[][] latinSquare1 = {{1, 2},{3, 4}};
-		int[] row1 = {1, 2};
-		
-		LatinSquare c = new LatinSquare(latinSquare1);
-		
-		assertEquals(true, Arrays.equals(row1, c.getRow(0)));
-	}
-	
-	@Test
-	public void hasAllValuesTest1() {
-		
-		int[] arr1 = {0, 1, 2, 3};
-		int[] arr2 = {3, 2, 1, 0};
-		
-		int[] arr3 = {1, 2, 3, 4};
-		int[] arr4 = {1, 2, 4, 3};
-		
-		assertEquals(true, LatinSquare.hasAllValues(arr1, arr2));
-		assertEquals(true, LatinSquare.hasAllValues(arr3, arr4));
-		
-	}
-	
-	@Test
-	public void hasAllValuesTest2() {
-		
-		int[] arr1 = {1, 2};
-		int[] arr2 = {2, 3};
-		
-		assertEquals(false, LatinSquare.hasAllValues(arr1, arr2));
-		
-	}
-	
-	@Test
-	public void hasDuplicatesTest() {
-		
-		int[] noDuplicates  = {0, 1, 2, 3, 4, 5, 6, 7};
-		int[] hasDuplicates = {0, 1, 2, 3, 3, 4, 5, 6};
-		
-		assertEquals(false, LatinSquare.hasDuplicates(noDuplicates));
-		assertEquals(true,  LatinSquare.hasDuplicates(hasDuplicates));
-		
-	}
-	
-	@Test
-	public void isLatinSquareTest1() {
-		
-		int[][] lsa1 = {{1, 2},{2, 2}};
-		int[][] lsa2 = {{2, 1},{2, 1}};
-		
-		LatinSquare ls1 = new LatinSquare(lsa1);
-		LatinSquare ls2 = new LatinSquare(lsa2);
-		
-		assertEquals(false, ls1.isLatinSquare());
-		assertEquals(false, ls2.isLatinSquare());
-		
-	}
-	
-	@Test
-	public void isLatinSquareTest2() {
-		
-		int[][] lsa3 = {{1, 2},{2, 3}};
-		LatinSquare ls3 = new LatinSquare(lsa3);
-		assertEquals(false, ls3.isLatinSquare());
-		
-	}
-	
-	@Test
-	public void isLatinSquareTest3() {
-		
-		int[][] lsa3 = {{3, 2},{2, 1}};
-		LatinSquare ls3 = new LatinSquare(lsa3);
-		assertEquals(false, ls3.isLatinSquare());
-		
-	}
-	
-	@Test
-	public void isLatinSquareTest4() {
-		
-		int[][] lsa3 = {{1, 2},{2, 1}};
-		LatinSquare ls3 = new LatinSquare(lsa3);
-		assertEquals(true, ls3.isLatinSquare());
-		
-	}
-	
-	@Test
-	public void setLatinSquareTest1() {
-		// TODO: Yisi will make this test.
-		
-		int[][] latinSquare1 = {{1, 2},{3, 4}};
-		
-		LatinSquare c = new LatinSquare();
-		
-		c.setLatinSquare(latinSquare1);
-		
-		assertEquals(latinSquare1, c.getLatinSquare());
-		
+	public void hasDuplicates_test1() {
+
+		LatinSquare LS = new LatinSquare();
+		int[] arr = { 1, 3, 5, 2, 4 };
+
+		boolean bHasDuplicates = LS.hasDuplicates(arr);
+
+		assertEquals(bHasDuplicates, false);
+
 	}
 
+	@Test
+	public void hasDuplicates_test2() {
+
+		LatinSquare LS = new LatinSquare();
+		int[] arr = { 1, 3, 5, 2, 3 };
+
+		boolean bHasDuplicates = LS.hasDuplicates(arr);
+
+		assertEquals(bHasDuplicates, true);
+
+	}
+
+	@Test
+	public void doesElementExist_Test1() {
+		LatinSquare LS = new LatinSquare();
+		int[] arr = { 1, 3, 5, 2, 4 };
+		int iValue = 3;
+		boolean bdoesElementExist = LS.doesElementExist(arr, iValue);
+
+		assertEquals(bdoesElementExist, true);
+	}
+
+	@Test
+	public void doesElementExist_Test2() {
+		LatinSquare LS = new LatinSquare();
+		int[] arr = { 1, 3, 5, 2, 4 };
+		int iValue = 99;
+		boolean bdoesElementExist = LS.doesElementExist(arr, iValue);
+
+		assertEquals(bdoesElementExist, false);
+	}
+
+	@Test
+	public void hasAllValues_Test1() {
+		LatinSquare LS = new LatinSquare();
+		int[] arr1 = { 1, 2, 3 };
+		int[] arr2 = { 3, 2, 1 };
+
+		boolean bhasAllValues = LS.hasAllValues(arr1, arr2);
+
+		assertEquals(bhasAllValues, true);
+
+	}
+
+	@Test
+	public void hasAllValues_Test2() {
+		LatinSquare LS = new LatinSquare();
+		int[] arr1 = { 1, 2, 3 };
+		int[] arr2 = { 3, 2, 99 };
+
+		boolean bhasAllValues = LS.hasAllValues(arr1, arr2);
+
+		assertEquals(bhasAllValues, false);
+
+	}
+
+	@Test
+	public void getColumn_Test1() {
+		
+		int[][] MySquare = { { 1, 2, 3 }, { 3, 1, 2 }, { 2, 3, 1 } };
+
+		LatinSquare LS = new LatinSquare(MySquare);
+		int [] ExpectedCol = {2,1,3};
+		int [] Col = LS.getColumn(1);
+		
+		assertTrue(Arrays.equals(ExpectedCol, Col));
+		System.out.println(Arrays.toString(Col));
+	}
+	
+	@Test
+	public void getRow_Test1() {
+		
+		int[][] MySquare = { { 1, 2, 3 }, { 3, 1, 2 }, { 2, 3, 1 } };
+
+		LatinSquare LS = new LatinSquare(MySquare);
+		int [] ExpectedRow = {3,1,2};
+		int [] Row = LS.getRow(1);
+		
+		assertTrue(Arrays.equals(ExpectedRow, Row));
+		System.out.println(Arrays.toString(Row));
+	}
+	
+	@Test
+	public void isLatinSquare_Test1() {
+		
+		int[][] MySquare = { { 1, 2, 3 }, { 3, 1, 2 }, { 2, 3, 1 } };
+
+		LatinSquare LS = new LatinSquare(MySquare);
+		
+		assertTrue(LS.isLatinSquare());
+	}
+	
+	@Test
+	public void isLatinSquare_Test2() {
+		
+		int[][] MySquare = { { 1, 2, 3 }, { 3, 1, 2 }, { 4, 5, 6 } };
+
+		LatinSquare LS = new LatinSquare(MySquare);
+		
+		assertFalse(LS.isLatinSquare());
+	}
 }
