7bf3e8b0a572fc01bb98b78ec79ffb32e5a32acb
James Villemarette <jvill@udel.edu>
2019-03-04 23:03:59 -0500

    finished lab 1

--- src/test/java/pkgHelper/LatinSquareTest.java
+++ src/test/java/pkgHelper/LatinSquareTest.java
@@ -2,6 +2,8 @@ package pkgHelper;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class LatinSquareTest {
@@ -46,17 +48,36 @@ public class LatinSquareTest {
 	
 	@Test
 	public void getColumnTest1() {
-		// TODO: Aris will make this test.
+		
+		int[][] latinSquare1 = {{1, 2},{3, 4}};
+		int[] col1 = {1, 3};
+		
+		LatinSquare c = new LatinSquare(latinSquare1);
+		
+		assertEquals(true, Arrays.equals(col1, c.getColumn(0)));
+		
 	}
 	
 	@Test
 	public void getLatinSquareTest1() {
 		// TODO: Yisi will make this test.
+		
+		int[][] LS1 = {{1, 2},{3, 4}};
+		LatinSquare data3 = new LatinSquare(LS1);
+		assertEquals(LS1, data3.getLatinSquare());
+		
 	}
 	
 	@Test
 	public void getRowTest1() {
 		// TODO: Yifan will make this test.
+		
+		int[][] latinSquare1 = {{1, 2},{3, 4}};
+		int[] row1 = {1, 2};
+		
+		LatinSquare c = new LatinSquare(latinSquare1);
+		
+		assertEquals(true, Arrays.equals(row1, c.getRow(0)));
 	}
 	
 	@Test
@@ -96,27 +117,57 @@ public class LatinSquareTest {
 	
 	@Test
 	public void isLatinSquareTest1() {
-		// TODO: Aris will make this test.
+		
+		int[][] lsa1 = {{1, 2},{2, 2}};
+		int[][] lsa2 = {{2, 1},{2, 1}};
+		
+		LatinSquare ls1 = new LatinSquare(lsa1);
+		LatinSquare ls2 = new LatinSquare(lsa2);
+		
+		assertEquals(false, ls1.isLatinSquare());
+		assertEquals(false, ls2.isLatinSquare());
+		
 	}
 	
 	@Test
 	public void isLatinSquareTest2() {
-		// TODO: Yifan will make this test.
+		
+		int[][] lsa3 = {{1, 2},{2, 3}};
+		LatinSquare ls3 = new LatinSquare(lsa3);
+		assertEquals(false, ls3.isLatinSquare());
+		
 	}
 	
 	@Test
 	public void isLatinSquareTest3() {
-		// TODO: Aris will make this test.
+		
+		int[][] lsa3 = {{3, 2},{2, 1}};
+		LatinSquare ls3 = new LatinSquare(lsa3);
+		assertEquals(false, ls3.isLatinSquare());
+		
 	}
 	
 	@Test
 	public void isLatinSquareTest4() {
-		// TODO: Yifan will make this test.
+		
+		int[][] lsa3 = {{1, 2},{2, 1}};
+		LatinSquare ls3 = new LatinSquare(lsa3);
+		assertEquals(true, ls3.isLatinSquare());
+		
 	}
 	
 	@Test
 	public void setLatinSquareTest1() {
 		// TODO: Yisi will make this test.
+		
+		int[][] latinSquare1 = {{1, 2},{3, 4}};
+		
+		LatinSquare c = new LatinSquare();
+		
+		c.setLatinSquare(latinSquare1);
+		
+		assertEquals(latinSquare1, c.getLatinSquare());
+		
 	}
 
 }
